25544e8d7692eb79564b9485b51eec2e81a050b3
Aleksei Mialkin <aleksei@mialkin.ru>
2024-09-13 18:49:11 +0300

    Add Kafka.Flow.Api project

---
+++ src/Kafka.Flow.Api/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.WriteTo.Console();
+});
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(options => { options.DescribeAllParametersInCamelCase(); });
+
+services.AddRouting(options => options.LowercaseUrls = true);
+
+var application = builder.Build();
+
+application.UseSerilogRequestLogging();
+
+application.UseSwagger();
+application.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+application.MapControllers();
+
+application.Run();
